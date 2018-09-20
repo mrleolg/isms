0619d31d75bf8d20d6c4f1387614783327b8b1b0
mrleolg <4329495+mrleolg@users.noreply.github.com>
2018-09-20 19:59:55 +0800

    aaa
    aaa

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,11 @@
 const path = require("path")
 const htmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
 const htmlWebpackPluginConfig = {
     title: "ISMS V1.0.0 开发版本",
     filename: "index.html",
-    template: "../index.html",
-    inject: head
+    template: "./src/template/index.html",
+    inject: "body"
 }
 
 module.exports = {
@@ -13,11 +14,39 @@ module.exports = {
         filename: "./dist/[hash]app.js",
         hashDigestLength: 6
     },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: {
+                        attrs: [":data-src"]
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name]2.[ext]",
+                            outputPath: "img/"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [new htmlWebpackPlugin(htmlWebpackPluginConfig)],
     devServer: {
         contentBase: path.join(__dirname, "./dist"),
         port: 9090,
-        open: true,
-        index: "index.html"
+        open: false,
+        index: "index.html",
+        inline: true,
+        hot: true,
+        compress: true
     }
 }
